caef1a55f7095126e6a03af3cca9ec58b2601854
vinay singh <vinaysinghcktd01@gmail.com>
2022-02-06 00:32:40 +0530

    list created of card transaction wiget and learned psoitional argument

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(tracking());
 }
 
 class tracking extends StatelessWidget {
+  final List<Transaction> my_transaction_list = [
+    Transaction(
+        id: 't1', amount: 69.099, date: DateTime.now(), title: "New shoes"),
+    Transaction(
+        id: 't2', amount: 639.099, date: DateTime.now(), title: "New bag"),
+    Transaction(
+        id: 't3', amount: 169.099, date: DateTime.now(), title: "New phone"),
+    Transaction(
+        id: 't4', amount: 89.099, date: DateTime.now(), title: "New Grocceries"),
+
+  ];
+
   @override
-Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-     home: Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter App"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter App"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity, // as much as he can take
+              child: Card(
+                // card by default depends upon the size of child unless they itself having a parent
+                color: Colors.indigoAccent,
+                child: Text(
+                  "For CHART",
+                  style: TextStyle(fontSize: 30),
+                ),
+                elevation: 20, //this is used for the shadow of the card
+              ),
+            ),
+            //A card in Flutter is in rounded corner shape and has a shadow. We mainly used it to store the content and action of a single object.
+           Column(
+             children: my_transaction_list.map((tx){
+               return Card(child: Text(tx.title),);
+             }).toList()
+           )
+          ],
+        ),
       ),
-       body: Column(children: <Widget>[
-         Card(child: Text("For CHART")),//A card in Flutter is in rounded corner shape and has a shadow. We mainly used it to store the content and action of a single object.
-         Card(child: Text("For the LIST"),)
-       ],),
-     ),
     );
   }
-
 }
---
+++ lib/transaction.dart
@@ -0,0 +1,15 @@
+// import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  @required
+  final String id; //required can be used by importing any material.dart , foundation.dart
+  @required
+  final String title;
+  @required
+  final double amount;
+  @required
+  final DateTime date;
+
+  Transaction({this.id, this.title, this.amount, this.date}); //  this is named positional arguments used in constrcutor and it can be design by using curly braces
+}
